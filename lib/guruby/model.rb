c31bdd3578d1c30db2bd642ff133bddd3c4db57e
Michael Mior <mmior@uwaterloo.ca>
2015-08-26 19:20:36 -0400

    Add constraints and fully working example

--- lib/guruby/model.rb
+++ lib/guruby/model.rb
@@ -16,14 +16,36 @@ module Guruby
       Gurobi.GRBaddvar @ptr, 0, nil, nil, obj, lb, ub, vtype.ord, varname
     end
 
+    # Add a new constraint to the model
+    def add_constraint(indexes, values, sense, rhs, name = '')
+      indexes_buffer = FFI::MemoryPointer.new :int, indexes.length
+      indexes_buffer.write_array_of_int indexes
+
+      values_buffer = FFI::MemoryPointer.new :double, values.length
+      values_buffer.write_array_of_double values
+
+      Gurobi.GRBaddconstr @ptr, indexes.length, indexes_buffer, values_buffer,
+                          sense.ord, rhs, name
+    end
+
     # Update the model
     def update
       Gurobi.GRBupdatemodel @ptr
     end
 
+    # Write the model to a file
+    def write(filename)
+      Gurobi.GRBwrite @ptr, filename
+    end
+
     # Set the sense of the model
     def set_sense(sense)
       Gurobi.GRBsetintattr @ptr, GRB_INT_ATTR_MODELSENSE, sense
+    end
+
+    # Optimize the model
+    def optimize
+      Gurobi.GRBoptimize @ptr
     end
 
     # Get the status of the model
--- test.rb
+++ test.rb
@@ -3,7 +3,18 @@ require_relative 'lib/guruby'
 env = Guruby::Environment.new
 model = Guruby::Model.new env
 
-model.add_var 0, 1, 0, GRB_INTEGER, 'var'
-model.update
+model.add_var 0, 1, 1, GRB_BINARY, 'x'
+model.add_var 0, 1, 1, GRB_BINARY, 'y'
+model.add_var 0, 1, 2, GRB_BINARY, 'z'
 model.set_sense GRB_MAXIMIZE
+model.update
+
+model.add_constraint [0, 1, 2], [1, 2, 3], GRB_LESS_EQUAL, 4.0, 'c0'
+model.add_constraint [0, 1], [1, 1], GRB_GREATER_EQUAL, 1.0, 'c1'
+model.update
+
+model.write '/tmp/test.lp'
+
+model.optimize
+
 p model.status
