821e0afa32b486d459a14e218a1396b530c10467
Michael Mior <mmior@uwaterloo.ca>
2016-01-16 14:04:13 -0500

    Fix alignment in GLPK struct

--- lib/mipper/glpk/ext/structs.rb
+++ lib/mipper/glpk/ext/structs.rb
@@ -1,28 +1,29 @@
 module MIPPeR
   module GLPK
+    # Integer optimization control parameters
     class IOCP < FFI::Struct
       layout :msg_lev, :int,
-        :br_tech, :int,
-        :bt_tech, :int,
-        :tol_int, :double,
-        :tol_obj, :double,
-        :tm_lim, :int,
-        :out_frq, :int,
-        :out_dly, :int,
-        :cb_func, callback([:pointer, :pointer], :void),
-        :cb_info, :pointer,
-        :cb_size, :int,
-        :pp_tech, :int,
-        :mip_gap, :double,
-        :mir_cuts, :int,
-        :gmi_cuts, :int,
-        :cov_cuts, :int,
-        :clq_cuts, :int,
-        :presolve, :int,
-        :binarize, :int,
-        :fp_heur, :int,
-        :alien, :int,
-        :foo_bar, [:double, 29]
+             :br_tech, :int,
+             :bt_tech, :int,
+             :tol_int, :double,
+             :tol_obj, :double,
+             :tm_lim, :int,
+             :out_frq, :int,
+             :out_dly, :int,
+             :cb_func, callback([:pointer, :pointer], :void),
+             :cb_info, :pointer,
+             :cb_size, :int,
+             :pp_tech, :int,
+             :mip_gap, :double,
+             :mir_cuts, :int,
+             :gmi_cuts, :int,
+             :cov_cuts, :int,
+             :clq_cuts, :int,
+             :presolve, :int,
+             :binarize, :int,
+             :fp_heur, :int,
+             :alien, :int,
+             :foo_bar, [:double, 29]
     end
   end
 end
